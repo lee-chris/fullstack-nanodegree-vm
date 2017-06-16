995668b043f401e9d4167277e6ee95ee27f4ec94
Chris Lee <lee.christopher.e@gmail.com>
2017-06-15 23:28:00 -0700

    added http handler for /restaurants
    this will list all of the restaurants in the db file.

--- vagrant/04_webserver_crud/webserver.py
+++ vagrant/04_webserver_crud/webserver.py
@@ -1,15 +1,36 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import cgi
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant
 
 class WebServerHandler(BaseHTTPRequestHandler):
 
+    engine = create_engine("sqlite:///restaurantmenu.db")
+    Base.metadata.bind = engine
+    DBSession = sessionmaker(bind=engine)
+    
     def do_GET(self):
         if self.path.endswith("/restaurants"):
+
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
+
             message = ""
+            message += "<html><body>"
+
+            session = self.DBSession()
+            for restaurant in session.query(Restaurant).all():
+                message += """
+  <div>
+    <h1>{0}</h1>
+    <a href="/restaurant/{1}/edit">Edit</a>
+    <a href="/restaurant/{1}/delete">Delete</a>
+  </div>""".format(restaurant.name, restaurant.id)
+
+            message += "</body></html>"
             
             self.wfile.write(message)
             print message
