60ca163db849d6eefabdd920a4850156adfba0a0
Chris Lee <lee.christopher.e@gmail.com>
2017-06-19 19:50:45 -0700

    added mechanisim to delete a Restaurant.

--- vagrant/04_webserver_crud/webserver.py
+++ vagrant/04_webserver_crud/webserver.py
@@ -81,6 +81,31 @@ class WebServerHandler(BaseHTTPRequestHandler):
             self.wfile.write(message)
             print message
             return
+
+        elif self.path.endswith("/delete"):
+
+            restaurant_id = self.path.split("/")[2]
+
+            session = self.DBSession()
+            restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            
+            message = """
+<html>
+  <body>
+    <form method="post" enctype="multipart/form-data">
+      Are you sure you want to delete {0}? <input type="submit" value="Delete"/>
+    </form>
+  </body>
+</html>
+""".format(restaurant.name)
+
+            self.wfile.write(message)
+            print message
+            return
         
         else:
             self.send_error(404, 'File Not Found: %s' % self.path)
@@ -128,7 +153,20 @@ class WebServerHandler(BaseHTTPRequestHandler):
             self.send_header('Location', '/restaurants')
             self.end_headers()
             
-        
+        elif self.path.endswith("/delete"):
+
+            restaurant_id = self.path.split("/")[2]
+
+            session = self.DBSession()
+            restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+
+            session.delete(restaurant)
+            session.commit()
+
+            self.send_response(302)
+            self.send_header('Location', '/restaurants')
+            self.end_headers()
+            
         else:
             self.send_error(404, 'File Not Found: %s' % self.path)
 
