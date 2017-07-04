48cbd5824c01f88a7a81f1a01d4099b08c25f14e
Chris Lee <lee.christopher.e@gmail.com>
2017-07-04 15:39:53 -0700

    added CRUD operations for restaurant level operations

--- vagrant/06_final_project/final_project.py
+++ vagrant/06_final_project/final_project.py
@@ -1,6 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from database_setup import Base, Restaurant, MenuItem
 
 app = Flask(__name__)
+engine = create_engine("sqlite:///restaurantmenu.db")
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
 
 # static db objects
 restaurants = [{"id" : 0, "name" : "Burger King"}, {"id" : 1, "name" : "Pizza Hut"}]
@@ -10,25 +18,63 @@ menu_items = [[{"id" : 0, "name" : "Whopper", "price" : "$2.99"}], [{"id" : 0, "
 @app.route("/restaurants")
 def view_restaurants():
 
+    # query for all restaurants
+    session = DBSession()
+    restaurants = session.query(Restaurant).all()
+    
     return render_template("restaurants.html", restaurants = restaurants)
 
 
-@app.route("/restaurants/new")
+@app.route("/restaurants/new", methods=["GET", "POST"])
 def create_restaurant():
 
-    return render_template("new_restaurant.html")
+    if request.method == "GET":
+        return render_template("new_restaurant.html")
+
+    elif request.method == "POST":
+
+        session = DBSession()
+        restaurant = Restaurant(name = request.form["name"])
+        session.add(restaurant)
+        session.commit()
+
+        return redirect(url_for("view_restaurants"))
 
 
-@app.route("/restaurants/delete/<int:restaurant_id>")
+@app.route("/restaurants/delete/<int:restaurant_id>", methods=["GET", "POST"])
 def delete_restaurant(restaurant_id):
 
-    return render_template("delete_restaurant.html", restaurant = restaurants[restaurant_id])
+    session = DBSession()
+    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+    
+    if request.method == "GET":
+        return render_template("delete_restaurant.html", restaurant = restaurant)
+
+    elif request.method == "POST":
+
+        session.delete(restaurant)
+        session.commit()
+
+        return redirect(url_for("view_restaurants"))
 
 
-@app.route("/restaurants/edit/<int:restaurant_id>")
+@app.route("/restaurants/edit/<int:restaurant_id>", methods=["GET", "POST"])
 def edit_restaurant(restaurant_id):
 
-    return render_template("edit_restaurant.html", restaurant = restaurants[restaurant_id])
+    session = DBSession()
+    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+
+    if request.method == "GET":
+        return render_template("edit_restaurant.html", restaurant = restaurant)
+
+    elif request.method == "POST":
+
+        restaurant.name = request.form["name"]
+
+        session.add(restaurant)
+        session.commit()
+
+        return redirect(url_for("view_restaurants"))
 
 
 @app.route("/restaurants/<int:restaurant_id>/menu")
