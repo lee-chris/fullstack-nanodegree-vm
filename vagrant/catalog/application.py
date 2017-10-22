77d53383699431b02a4d13a914fadc7f2b484b8d
Chris Lee <lee.christopher.e@gmail.com>
2017-10-21 18:49:30 -0700

    added initial home page.

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -1,6 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+from item_dao import ItemDao
 
 app = Flask(__name__)
+item_dao = ItemDao()
+
+@app.route("/")
+def view_product_catalog():
+    
+    items = item_dao.get_items()
+    return render_template("catalog.html", items = items)
+
 
 if __name__ == "__main__":
     app.secret_key = "super_secret_key"
